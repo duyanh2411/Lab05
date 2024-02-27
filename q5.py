9f05537a0d16d68f8b1ff6bfd9f4d662068e3106
duyanh2411 <155623710+duyanh2411@users.noreply.github.com>
2024-02-27 23:05:42 +0700

    q5.py

--- q5.py
+++ q5.py
@@ -1,12 +1,12 @@
-def dem_so_lan_xuat_hien(duong_chuoi)
+def dem_so_lan_xuat_hien(dong_chu)
     dem = {}
-    for ki_tu in duong_chuoi: 
+    for ki_tu in dong_chu: 
         if ki_tu != ' ':
             dem[ki_tu] = dem.get(ki_tu, 0) + 1
 
     for ki_tu, so_lan in dem.items():
         print(f"{ki_tu}: {so_lan} lần")
 
-dong_chuoi = input("Nhập dòng chữ: ")
+dong_chu = input("Nhập dòng chữ: ")
 
-dem_so_lan_xuat_hien(dong_chuoi)
+dem_so_lan_xuat_hien(dong_chu)
